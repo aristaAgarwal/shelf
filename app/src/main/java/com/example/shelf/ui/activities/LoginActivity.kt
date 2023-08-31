3b6fbb49ee96928a034626b73f5879ff548203ee
arista.agarwal <arista.agarwal@dish.com>
2023-08-31 20:27:46 +0530

    "Google and otp sign in removed"

--- app/src/main/java/com/example/shelf/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/shelf/ui/activities/LoginActivity.kt
@@ -1,15 +1,39 @@
 package com.example.shelf.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.example.shelf.R
-import com.example.shelf.ui.fragments.SignIn
+import com.example.shelf.constant.AppPreferences
+import com.example.shelf.databinding.ActivityLogin2Binding
+import com.example.shelf.ui.fragments.SignUp
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLogin2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login2)
+        binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.btn.setOnClickListener {
+            if (AppPreferences(this).getPass() == binding.pass.text.toString()){
+                AppPreferences(this).setUsername(binding.name.text.toString())
+                binding.errorCred.isVisible = false
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                this.finish()
+            }
+            else
+            {
+                binding.errorCred.isVisible = true
+            }
+        }
     }
 
     fun onClick(view: View) {
@@ -17,7 +41,7 @@ class LoginActivity : AppCompatActivity() {
 
             R.id.sign_up -> {
                 val fragmentTransaction = supportFragmentManager.beginTransaction()
-                fragmentTransaction.add(R.id.frameLayout, SignIn())
+                fragmentTransaction.add(R.id.frameLayout, SignUp())
                 fragmentTransaction.addToBackStack(null)
                 fragmentTransaction.commit()
             }
